5cc71eb83db110a708b8e81628efded04ae6c4aa
Chase Ure <urefast@msn.com>
2020-05-06 23:24:37 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -8,9 +8,9 @@ else
 end
 
 
-def not_safe?(speed)
-  "speed" > 60? "true" : "false"
-  "speed" < 40? "true" : "false"
+def not_safe? = speed
+  speed > 60? "true" : "false"
+  speed < 40? "true" : "false"
 end
 end
 
